17c195353f4c9fd12807512acb546bbaa4c66eae
Tomas <tomas.e.gear@gmail.com>
2020-03-26 00:04:03 -0400

    update logic to display whether word is palindrome or not

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,44 +1,39 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-console.log("page loaded"); 
-
-// $(document).on("click", "#checkWord", function(e) {
-//     e.preventDefault(); 
-//     console.log("button clicked"); 
-
-// }); 
-
 let word = ""; 
 
 $("#checkWord").click(function(e) {
     e.preventDefault(); 
-    console.log("button clicked"); 
     // call checkPalindrome function 
     word = $("#palindrome").val().trim(); 
-    console.log("submitted word is: ", word); 
 
-    checkPalindrome(word).then(displayResult());
+    console.log("++++++++++++++++++");
 
+    checkPalindrome(word).then(displayResult(word));
+    $("#palindrome").val(""); 
 })
 
-let palindrome = true;  
+let palindrome;  
+
     
 let checkPalindrome = function(text) {
 
     return new Promise(function(resolve, reject) {
 
-        console.log("word before: ", text); 
+        // console.log("word before: ", text); 
 
         word = text.toLowerCase().replace(/[^A-Za-z0-9]/g, ''); 
     
         //find a way to eliminate spaces, upper and lower case letters, punctuation 
-        console.log("checkPalindrome called"); 
-        console.log("word after: ", word); 
+        // console.log("checkPalindrome called"); 
+        // console.log("word after: ", word); 
     
         let leftPointer = 0; 
         let rightPointer = word.length -1; 
         console.log("Palindrome before while loop: ", palindrome); 
-    
+        
+        palindrome = true; 
+
         while (leftPointer < rightPointer) {
     
             if(word[leftPointer] !== word[rightPointer]) {
@@ -57,23 +52,21 @@ let checkPalindrome = function(text) {
 
 };
 
-let displayResult = function() {
+let displayResult = function(word) {
     
     console.log("palindrome variable in displayResult: ", palindrome); 
-    let word = "word"; 
+    // console.log("user submitted word: ", word); 
 
     if (palindrome) {
-        console.log("displayResult function called: palindrome is true"); 
+        // console.log("displayResult function called: palindrome is true"); 
 
-        $("#palindromeResult").text(`Yes ${word} is a palindrome!`);
+        $("#palindromeResult").text(`Yes, ${word} is a palindrome!`);
     } else {
-        console.log("displayResult function called: palindrome is false"); 
-        $("#palindromeResult").text("Nope!");
+        // console.log("displayResult function called: palindrome is false"); 
+        $("#palindromeResult").text(`Nope, ${word} is not a palindrome!`);
     }
 
 }
-
-
 
 });
 
