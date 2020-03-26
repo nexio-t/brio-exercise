de46c1c934399713fc19c568a59c844613fa53cd
Tomas <tomas.e.gear@gmail.com>
2020-03-25 23:43:46 -0400

    add function to check if user submission is palindrome

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,7 +2,6 @@ document.addEventListener("DOMContentLoaded", function() {
 
 console.log("page loaded"); 
 
-
 // $(document).on("click", "#checkWord", function(e) {
 //     e.preventDefault(); 
 //     console.log("button clicked"); 
@@ -21,6 +20,42 @@ $("#checkWord").click(function(e) {
     checkPalindrome(word).then(displayResult());
 
 })
+
+let palindrome = true;  
+    
+let checkPalindrome = function(text) {
+
+    return new Promise(function(resolve, reject) {
+
+        console.log("word before: ", text); 
+
+        word = text.toLowerCase().replace(/[^A-Za-z0-9]/g, ''); 
+    
+        //find a way to eliminate spaces, upper and lower case letters, punctuation 
+        console.log("checkPalindrome called"); 
+        console.log("word after: ", word); 
+    
+        let leftPointer = 0; 
+        let rightPointer = word.length -1; 
+        console.log("Palindrome before while loop: ", palindrome); 
+    
+        while (leftPointer < rightPointer) {
+    
+            if(word[leftPointer] !== word[rightPointer]) {
+                palindrome = false
+            } 
+            leftPointer++; 
+            rightPointer--; 
+    
+        }
+
+        console.log("Palindrome after while loop: ", palindrome); 
+        
+        resolve(palindrome);
+     
+    }); 
+
+};
 
 
 });
