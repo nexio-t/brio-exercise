66afcf04f7a7d67cf98e7afaf25c0d13191fab50
Tomas <tomas.e.gear@gmail.com>
2020-03-26 09:42:49 -0400

    center button and standardize height of cards with panel

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,72 +1,63 @@
 document.addEventListener("DOMContentLoaded", function() {
+  let word = "";
+  let palindrome;
 
-let word = ""; 
-
-$("#checkWord").click(function(e) {
-    e.preventDefault(); 
-    // call checkPalindrome function 
-    word = $("#palindrome").val().trim(); 
+  $("#checkWord").click(function(e) {
+    e.preventDefault();
+    // call checkPalindrome function
+    word = $("#palindrome")
+      .val()
+      .trim();
 
     console.log("++++++++++++++++++");
 
     checkPalindrome(word).then(displayResult(word));
-    $("#palindrome").val(""); 
-})
+    $("#palindrome").val("");
+  });
 
-let palindrome;  
-
-    
-let checkPalindrome = function(text) {
-
+  let checkPalindrome = function(text) {
     return new Promise(function(resolve, reject) {
+      // console.log("word before: ", text);
 
-        // console.log("word before: ", text); 
+      let textOnly = text.toLowerCase().replace(/[^A-Za-z0-9]/g, "");
 
-        word = text.toLowerCase().replace(/[^A-Za-z0-9]/g, ''); 
-    
-        //find a way to eliminate spaces, upper and lower case letters, punctuation 
-        // console.log("checkPalindrome called"); 
-        // console.log("word after: ", word); 
-    
-        let leftPointer = 0; 
-        let rightPointer = word.length -1; 
-        console.log("Palindrome before while loop: ", palindrome); 
-        
-        palindrome = true; 
+    //   textOnly = text.toLowerCase().replace(/[^A-Za-z0-9]/g, "");
 
-        while (leftPointer < rightPointer) {
-    
-            if(word[leftPointer] !== word[rightPointer]) {
-                palindrome = false
-            } 
-            leftPointer++; 
-            rightPointer--; 
-    
+      //find a way to eliminate spaces, upper and lower case letters, punctuation
+      // console.log("checkPalindrome called");
+      // console.log("textOnly after: ", textOnly);
+
+      let leftPointer = 0;
+      let rightPointer = textOnly.length - 1;
+      console.log("Palindrome before while loop: ", palindrome);
+
+      palindrome = true;
+
+      while (leftPointer < rightPointer) {
+        if (textOnly[leftPointer] !== textOnly[rightPointer]) {
+          palindrome = false;
         }
+        leftPointer++;
+        rightPointer--;
+      }
 
-        console.log("Palindrome after while loop: ", palindrome); 
-        
-        resolve(palindrome);
-     
-    }); 
+      console.log("Palindrome after while loop: ", palindrome);
 
-};
+      resolve(palindrome);
+    });
+  };
 
-let displayResult = function(word) {
-    
-    console.log("palindrome variable in displayResult: ", palindrome); 
-    // console.log("user submitted word: ", word); 
+  let displayResult = function(word) {
+    console.log("palindrome variable in displayResult: ", palindrome);
+    // console.log("user submitted word: ", word);
 
     if (palindrome) {
-        // console.log("displayResult function called: palindrome is true"); 
+      // console.log("displayResult function called: palindrome is true");
 
-        $("#palindromeResult").text(`Yes, ${word} is a palindrome!`);
+      $("#palindromeResult").text(`Yes, ${word} is a palindrome!`);
     } else {
-        // console.log("displayResult function called: palindrome is false"); 
-        $("#palindromeResult").text(`Nope, ${word} is not a palindrome!`);
+      // console.log("displayResult function called: palindrome is false");
+      $("#palindromeResult").text(`Nope, ${word} is not a palindrome!`);
     }
-
-}
-
+  };
 });
-
